3857bac91e3b1941c8b1580903558f3dc79290ae
sofisalmanarif <sofisalman9906@gmail.com>
2024-11-23 12:57:48 +0530

    Composite elements managed

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
-const Popup = ({metaData,setIsDialogueOpen}) => {
-    console.log("metadata",metaData)
+const Popup = ({metaData={},setIsDialogueOpen}) => {
+    console.log("metadata in popup",metaData[0]?.coordinates[0][0])
   return (
-    <div className="bg-white flex-col text-black h-80    rounded-md flex  w-[500px] absolute top-4 left-10">
+    <div className="bg-white shadow-lg mt-20 flex-col text-black h-92 py-4 px-6   rounded-md flex  w-[700px] absolute top-2 left-[36%]">
         <div className='flex flex-col justify-between relative'>
-        <span  onClick={()=>setIsDialogueOpen((prev)=>!prev)} className='   bg-red-500 h-8  w-8 m-4 absolute top-2 right-2 flex text-white font-bold items-center justify-center rounded-full'>x</span>
-            <div className="flex justify-center pt-24 w-full h-full flex-col items-center">
-            <span> Top : {metaData[0].top}</span>
-            <span> height : {metaData[0].height}</span>
-            <span> left : {metaData[0].left}</span>
-            <span> width : {metaData[0].width}</span>
+        <span  onClick={()=>setIsDialogueOpen((prev)=>!prev)} className=' h-8  w-8 m-2 absolute top-0 -right-4 flex  font-bold items-center justify-center rounded-full'>
+            <img src="../../public/Flat_cross_icon.svg.png" alt="" />
+        </span>
+            <div className="flex text-lg font-semibold text-zinc-600  pt-16 w-full h-full flex-col ">
+            <span> Page Number : {metaData[0].page_number}</span>
+            <span> Layout Width : {metaData[0].layout_width}</span>
+            <span> Layout Height : {metaData[0].layout_height}</span>
+            <span> Cordinates : </span>
+            <span> Top Left : X = {metaData[0]?.coordinates[0][0]} ,Y = {metaData[0]?.coordinates[0][1]}  </span>
+            <span> Bottom Left : X = {metaData[0]?.coordinates[1][0]} ,Y = {metaData[0]?.coordinates[1][1]}  </span>
+            <span> Bottom Right : X = {metaData[0]?.coordinates[2][0]} ,Y = {metaData[0]?.coordinates[2][1]}  </span>
+            <span> Top Right : X = {metaData[0]?.coordinates[3][0]} ,Y = {metaData[0]?.coordinates[3][1]}  </span>
+            
             </div>
         </div>
           </div>
--- src/pages/ViewPdf.jsx
+++ src/pages/ViewPdf.jsx
@@ -17,50 +17,85 @@ const ViewPdf = () => {
   const { filename } = useParams();
   const [pdfData, setPdfData] = useState(null);
   const [cordinatesArray, setCordinatesArray] = useState(null);
+  const [metaData, setMetaData] = useState(null)
 
   const [isDilogueOpen,setIsDialogueOpen] = useState(false)
   
 
   function findElement(id) {
+   if(pdfData[0].type ==="Title"){
     let element = pdfData
-      .filter((element) => element.element_id === id)
-      .map((element) => element.metadata);
-    let extractedData = element.map((item) => ({
-      coordinates: item.coordinates.points,
-      page_number: item.page_number,
-      layout_width: item.coordinates.layout_width,
-      layout_height: item.coordinates.layout_height,
-    }));
-    console.log(extractedData[0]);
+    .filter((element) => element.element_id === id)
+    .map((element) => element.metadata);
+  let extractedData = element.map((item) => ({
+    coordinates: item.coordinates.points,
+    page_number: item.page_number,
+    layout_width: item.coordinates.layout_width,
+    layout_height: item.coordinates.layout_height,
+  }));
+  console.log("extracted",extractedData[0]);
+  setMetaData(extractedData)
+  // console.log("now meta data",metaData)
 
-    //   console.log("top" ,(extractedData[0].coordinates[0][1]/(extractedData[0].layout_height/100)),extractedData[0].layout_height);
+  const transformedData = extractedData.map((item) => {
+    const top = item.coordinates[0][1] / (item.layout_height / 100); // Top as percentage
+    const height =
+      (item.coordinates[1][1] - item.coordinates[0][1]) /
+      (item.layout_height / 100); // Height as percentage
+    const width =
+      (item.coordinates[2][0] - item.coordinates[0][0]) /
+      (item.layout_width / 100); // Width as percentage
+    const left = item.coordinates[0][0] / (item.layout_width / 100); // Left as percentage
 
-    //   console.log("height" ,((extractedData[0].coordinates[1][1] - extractedData[0].coordinates[0][1])/(extractedData[0].layout_height/100)),extractedData[0].layout_height);
+    return {
+      pageIndex: item.page_number - 1,
+      top,
+      height,
+      width,
+      left,
+    };
+  });
 
-    //   console.log("width" ,((extractedData[0].coordinates[2][0] - extractedData[0].coordinates[1][0])/(extractedData[0].layout_width/100)),extractedData[0].layout_width);
-
-    //   console.log("left",(extractedData[0].coordinates[0][0]/(extractedData[0].layout_width/100)),extractedData[0].layout_width)
-    const transformedData = extractedData.map((item) => {
-      const top = item.coordinates[0][1] / (item.layout_height / 100); // Top as percentage
-      const height =
-        (item.coordinates[1][1] - item.coordinates[0][1]) /
-        (item.layout_height / 100); // Height as percentage
-      const width =
-        (item.coordinates[2][0] - item.coordinates[0][0]) /
-        (item.layout_width / 100); // Width as percentage
-      const left = item.coordinates[0][0] / (item.layout_width / 100); // Left as percentage
-
+  setCordinatesArray(transformedData);
+  console.log("cord-array",cordinatesArray);
+  
+   }
+   else if(pdfData[0].type ==="CompositeElement"){
+    console.log("alag json hai")
+    console.log("pdf-data",pdfData);
+    const extractedData = pdfData.filter((element)=>element.element_id ==id)[0].metadata.map((smallElement)=>smallElement)
+    console.log("extractedData",extractedData)
+    const metadata= extractedData.map((elem)=> 
+      ( 
+      {coordinates:elem.metadata.coordinates,
+      page_number : elem.metadata.page_number
+    }))
+    setMetaData(metadata)
+    console.log("metadata",metadata)
+    // let extraxtedMetaData = metadata.map((item) => (item.page_number,item.coordinates))
+    //  metadata.map((item) => item.points.map((item,idx)=>console.log(idx ,item)))
+    const resultArray = metadata.map((item) => {
+      console.log("item",item.coordinates.layout_height)
+      const points = item.coordinates.points;
+      const top = points[0][1]/(item.coordinates.layout_height/100); // y-coordinate of the top-left point
+      const left = points[0][0]/(item.coordinates.layout_width/100); // x-coordinate of the top-left point
+      const height = (points[1][1] - points[0][1])/(item.coordinates.layout_height/100); // Difference in y-coordinates
+      const width = (points[2][0] - points[0][0])/(item.coordinates.layout_width/100); // Difference in x-coordinates
+    
       return {
-        pageIndex: item.page_number - 1,
         top,
+        left,
         height,
         width,
-        left,
+        pageIndex: item.page_number - 1, // Convert to 0-based index
       };
     });
-
-    setCordinatesArray(transformedData);
-    console.log(cordinatesArray);
+    
+    console.log(resultArray);
+    setCordinatesArray(resultArray)
+    console.log("cordarray",cordinatesArray)
+    
+   }
   }
 
  
@@ -72,7 +107,7 @@ const ViewPdf = () => {
         `https://pdfserver-x314vhv1.b4a.run/data/${filename}`
       );
       setPdfData(data);
-      //   console.log(data);
+        console.log("json data",data[0].type);
     } catch (error) {
       console.error("Error fetching PDF data:", error);
     }
@@ -93,7 +128,7 @@ const ViewPdf = () => {
           </Link>
           <h1 className="text-3xl font-bold text-center mt-10">{filename}</h1>
         </div>
-        <div className="h-screen mt-5">
+        <div className="h-full  mt-5">
           {pdfData ? (
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
               {cordinatesArray ? (
@@ -102,7 +137,7 @@ const ViewPdf = () => {
                   areas={cordinatesArray}
                 />
               ) : (
-                <Viewer fileUrl={`https://pdfserver-x314vhv1.b4a.run/files/${filename}`} />
+                <Viewer theme={Popup} fileUrl={`https://pdfserver-x314vhv1.b4a.run/files/${filename}`} />
               )}
             </Worker>
           ) : (
@@ -110,24 +145,27 @@ const ViewPdf = () => {
           )}
         </div>
       </div>
-      <div className="w-[30%] relative  py-10 px-10 h-screen overflow-y-auto">
+      <div className="w-[30%]   my-20 py-2 px-10 h-screen overflow-y-auto">
         {pdfData &&
           pdfData.map((element) => (
-            <div className="flex  justify-between items-center">
-            <p
+            <div onClick={() => findElement(element.element_id)} className="flex   justify-between items-center bg-zinc-100 hover:bg-zinc-300 mb-2 rounded-md">
+              <div  className=" w-[90%] flex mb-2 rounded-md px-2">
+              <p
               onClick={() => findElement(element.element_id)}
               key={element.element_id}
-              className="text-lg max-w-[90%] my-10 text text-zinc-300 hover:font-bold hover:text-white ease-in-out duration-500"
+              className="text-lg max-w-[90%] my-10 text   hover:font hover:text-black ease-in-out duration-500"
             >
               {element.text}
             </p>
-              <span className="cursor-pointer font-semibold p-4" onClick={()=>setIsDialogueOpen((prev)=>!prev)} >. . .</span>
+              </div>
+           
+              <span className="cursor-pointer font-extrabold p-4" onClick={()=>setIsDialogueOpen((prev)=>!prev)} >. . .</span>
 
               
             </div>
           ))}
 {
-                isDilogueOpen && <Popup metaData={cordinatesArray} setIsDialogueOpen={setIsDialogueOpen}/>
+                isDilogueOpen && <Popup metaData={metaData} setIsDialogueOpen={setIsDialogueOpen}/>
               }
           
       </div>
@@ -136,3 +174,28 @@ const ViewPdf = () => {
 };
 
 export default ViewPdf;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //   console.log("top" ,(extractedData[0].coordinates[0][1]/(extractedData[0].layout_height/100)),extractedData[0].layout_height);
+
+    //   console.log("height" ,((extractedData[0].coordinates[1][1] - extractedData[0].coordinates[0][1])/(extractedData[0].layout_height/100)),extractedData[0].layout_height);
+
+    //   console.log("width" ,((extractedData[0].coordinates[2][0] - extractedData[0].coordinates[1][0])/(extractedData[0].layout_width/100)),extractedData[0].layout_width);
+
+    //   console.log("left",(extractedData[0].coordinates[0][0]/(extractedData[0].layout_width/100)),extractedData[0].layout_width)
